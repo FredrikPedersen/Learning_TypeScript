c8f9ee0cca800453db7e784586fe7f01e6e46880
FredrikPedersen <fredrikhp@gmail.com>
2020-09-05 21:36:41 +0200

    Completed Section 2: Part 20: Working With Enums

--- Course_Project/src/script/app.ts
+++ Course_Project/src/script/app.ts
@@ -1,9 +1,24 @@
-const person: {
-    name: string;
-    age: number;
-} = {
+enum  Role {
+    ADMIN,
+    READ_ONLY,
+    AUTHOR
+}
+
+const person = {
     name: "Fredrik",
-    age: 25
+    age: 25,
+    hobbies: ["Sports", "Cooking"],
+    role: Role.ADMIN
 };
 
-console.log(person.age);
+let favoriteActivities: string[];
+favoriteActivities = ["Video Games", "Gym"];
+
+person.hobbies.forEach(hobby => {
+   console.log(hobby.toUpperCase());
+});
+
+
+if (person.role == Role.ADMIN) {
+    console.log("HE IS AN ADMIN")
+}
