5e0530f9018559a979c6ceb8ed0fceb8ec601c6b
FredrikPedersen <fredrikhp@gmail.com>
2020-09-05 21:55:48 +0200

    Completed Section 2: Part 23: Literal Types

--- Course_Project/src/script/app.ts
+++ Course_Project/src/script/app.ts
@@ -1,13 +1,20 @@
-const combine = (value1: number | string, value2: number | string): number | string => {
-    if (typeof value1 == "number" && typeof value2 == "number") {
-        return value1 + value2;
+const combine = (
+    value1: number | string,
+    value2: number | string,
+    resultConversion: "as-number" | "as-text"
+): number | string => {
+    if (typeof value1 == "number" && typeof value2 == "number" || resultConversion === "as-number") {
+        return +value1 + +value2;
     } else {
         return value1.toString() + " " + value2.toString();
     }
 };
 
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
 
-const combinedNames = combine("Fredrik", "Thomas");
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+
+const combinedNames = combine("Fredrik", "Thomas", "as-text");
 console.log(combinedNames);
