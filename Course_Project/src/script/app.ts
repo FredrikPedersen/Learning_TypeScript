81763a9a7c504f007723e49e9215d33023e7f1d4
Fredrik Pedersen <fredrikhp@gmail.com>
2020-09-11 16:53:11 +0200

    Completed Section 5: Part 62: "private" and "public" Access Modifiers

--- Course_Project/src/script/app.ts
+++ Course_Project/src/script/app.ts
@@ -1,6 +1,7 @@
 class Department {
 
-    name: string;
+    private name: string;
+    private employees: string[] = [];
 
     constructor(name: string) {
         this.name = name;
@@ -8,9 +9,22 @@ class Department {
 
     describe(this: Department) {
         console.log("Department: " + this.name);
+        console.log("Employees: " + this.employees);
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
 
 const classInstance = new Department("Orakel");
+classInstance.addEmployee("Fredrik");
+classInstance.addEmployee("Ana-Maria");
 
+classInstance.printEmployeeInformation()
 classInstance.describe()
