059119a082d0d1e93427fdf7455882dc19d317a1
FredrikPedersen <fredrikhp@gmail.com>
2020-10-21 18:22:50 +0200

    Completed Section 8: Part 108: Adding Multiple Decorators

--- Course_Project/src/script/app.ts
+++ Course_Project/src/script/app.ts
@@ -1,4 +1,4 @@
-/* ----- Part 105 - 107: Intro to Decorators ----- */
+/* ----- Part 105 - 108: Intro to Decorators ----- */
 
 // Conventions dictate decorator-functions start with upper-case names
 function Logger(logString: string): Function {
@@ -20,7 +20,8 @@ function WithTemplate(template: string, hookId: string) {
     }
 }
 
-//The @-symbol is a special identifier Typescript recognizes to point to a decorator function.
+// The @-symbol is a special identifier Typescript recognizes to point to a decorator function.
+// When multiple loggers are applied, they are executed bottom up. In this case WithTemplate runs first.
 @Logger("LOGGING - PERSON")
 @WithTemplate("<h1>My Person Object</h1>", "app")
 class PersonWithDecorators {
