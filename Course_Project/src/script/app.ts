f3b972c435fa547b7d3621f66d1bff5bc1066a86
FredrikPedersen <fredrikhp@gmail.com>
2020-10-21 18:59:19 +0200

    Completed Section 8: Part 112: Returning (and changing) a Class in a Class Decorator

--- Course_Project/src/script/app.ts
+++ Course_Project/src/script/app.ts
@@ -2,38 +2,24 @@
 
 // Conventions dictate decorator-functions start with upper-case names
 // Constructor is an argument a Decorator receives when applied to a class
+import instantiate = WebAssembly.instantiate;
+
 function Logger(logString: string): Function {
-    return function(constructor: Function) {
+    return function (constructor: Function) {
         console.log(logString);
         console.log(constructor);
-    }
-}
-
-function WithTemplate(template: string, hookId: string) {
-    return function (constructor: any) {
-        const hookElement = document.getElementById(hookId);
-        const person = new constructor();
-
-        if (hookElement) {
-            hookElement.innerHTML = template;
-            hookElement.querySelector("h1")!.textContent = person.name;
-        }
     }
 }
 
 // The @-symbol is a special identifier Typescript recognizes to point to a decorator function.
 // When multiple loggers are applied, they are executed bottom up. In this case WithTemplate runs first.
 @Logger("LOGGING - PERSON")
-@WithTemplate("<h1>My Person Object</h1>", "app")
 class PersonWithDecorators {
     name: String = "Fredrik";
 
     constructor() {
-        console.log("Creating person object...");
     }
 }
-
-const classWithDecorator = new PersonWithDecorators();
 
 /* ----- Part 109 and 110: Property, Accessor and Parameter Decorators ----- */
 
@@ -92,3 +78,35 @@ class Product {
     }
 }
 
+/* ----- Part 112: Returning and Changing a Class in A Decorator ----- */
+
+// Returns a new class with the template when the class is instantiated. Note how it extends the original class by
+// extending the originalConstructor.
+function WithTemplate(template: string, hookId: string) {
+    return function<T extends {new(...args: any[]): {name: string} }> (originalConstructor: T) {
+
+        return class extends originalConstructor {
+            constructor(...args: any[]) {
+                super();
+
+                const hookElement = document.getElementById(hookId);
+
+                if (hookElement) {
+                    hookElement.innerHTML = template;
+                    hookElement.querySelector("h1")!.textContent = this.name;
+                }
+            }
+        }
+    };
+}
+
+@WithTemplate("<h1>My Person Object</h1>", "app")
+class DecoratedWithReturnValue {
+
+    name: string = "Fredrik";
+
+    constructor() {
+    }
+}
+
+const instantiatedClass: DecoratedWithReturnValue = new DecoratedWithReturnValue();
