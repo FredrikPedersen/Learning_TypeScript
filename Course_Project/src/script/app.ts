8d3fa047f0c00f02a4c00676b639e9298520aefa
FredrikPedersen <fredrikhp@gmail.com>
2020-10-21 18:32:24 +0200

    Completed Section 8: Part 109: Diving into Property Decorators

--- Course_Project/src/script/app.ts
+++ Course_Project/src/script/app.ts
@@ -1,6 +1,7 @@
 /* ----- Part 105 - 108: Intro to Decorators ----- */
 
 // Conventions dictate decorator-functions start with upper-case names
+// Constructor is an argument a Decorator receives when applied to a class
 function Logger(logString: string): Function {
     return function(constructor: Function) {
         console.log(logString);
@@ -33,3 +34,35 @@ class PersonWithDecorators {
 }
 
 const classWithDecorator = new PersonWithDecorators();
+
+/* ----- Part 109: Diving Into Property Decorators ----- */
+
+// target and propertyName are a properties a Decorator receives when applied to a class property
+function Log(target: any, propertyName: string | symbol) {
+    console.log("Property Decorator");
+    console.log(target, propertyName);
+}
+
+class Product {
+
+    @Log
+    title: string;
+    private _price: number;
+
+    constructor(title: string, price: number) {
+        this.title = title;
+        this._price = price;
+    }
+
+    getPriceWithTax(tax: number) {
+        return this._price * (1 + tax);
+    }
+
+    set price(price: number) {
+        if (price > 0) {
+            this._price = price;
+        } else {
+            throw new Error("Invalid price - should be positive!");
+        }
+    }
+}
