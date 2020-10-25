953f8c8486b90d2cfa2dc672a9f8c043182d4031
FredrikPedersen <fredrikhp@gmail.com>
2020-10-25 15:52:25 +0100

    Completed Section 9: Part 127: Rendering Project Lists

--- DragAndDropProject/src/script/app.ts
+++ DragAndDropProject/src/script/app.ts
@@ -1,4 +1,5 @@
 /* ----- Validation ----- */
+
 interface Validatable {
     value: string | number;
     required?: boolean;
@@ -37,6 +38,7 @@ function validate(validatables: Validatable[]) : boolean {
 }
 
 /* ----- Decorators ----- */
+
 function Autobind(target: any, methodName: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
     const adjustedDescriptor: PropertyDescriptor = {
@@ -50,6 +52,35 @@ function Autobind(target: any, methodName: string, descriptor: PropertyDescripto
 }
 
 /*----- Classes ----- */
+
+class ProjectList {
+
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    sectionElement: HTMLElement;
+
+    constructor(private type: "active" | "finished") {
+        this.templateElement = document.getElementById("project-list")! as HTMLTemplateElement;
+        this.hostElement = document.getElementById("app")! as HTMLDivElement;
+        this.sectionElement = document.getElementById("projects")! as HTMLElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.sectionElement = importedNode.firstElementChild as HTMLElement;
+        this.sectionElement.id = `${this.type}-projects`;
+
+        this.attachToDOM();
+        this.renderContent();
+    }
+
+    private renderContent() {
+        this.sectionElement.querySelector("ul")!.id = `${this.type}-projects-list`;
+        this.sectionElement.querySelector("h2")!.textContent = this.type.toUpperCase() + " PROJECTS";
+    }
+
+    private attachToDOM() {
+        this.hostElement.insertAdjacentElement("beforeend", this.sectionElement);
+    }
+}
 
 class ProjectInput {
 
@@ -73,7 +104,7 @@ class ProjectInput {
         this.peopleInputElement = this.formElement.querySelector("#people")! as HTMLInputElement;
 
         this.configure();
-        this.attach();
+        this.attachToDOM();
     }
 
     private gatherUserInput(): [string, string, number] | undefined {
@@ -125,9 +156,11 @@ class ProjectInput {
         this.formElement.addEventListener("submit", this.submitHandler);
     }
 
-    private attach() {
+    private attachToDOM() {
         this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
     }
-}
+} // Project Input
 
 const projectInput = new ProjectInput();
+const activeProjectList = new ProjectList("active");
+const finishedProjectList = new ProjectList("finished");
