c1fb1122ac17b804d05d65d118dfbb7205ecf7ae
FredrikPedersen <fredrikhp@gmail.com>
2020-10-28 18:43:42 +0100

    Added Dotenv-webpack to be able to locally store API-key

--- SelectSharePlace/src/app.ts
+++ SelectSharePlace/src/app.ts
@@ -1,9 +1,13 @@
+const API_KEY = process.env.API_KEY;
+
 const form = document.querySelector("form")! as HTMLFormElement;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
 function searchAddressHandler(event: Event) {
     event.preventDefault();
     const enteredAdress = addressInput.value;
+    console.log(enteredAdress);
+    console.log(API_KEY);
 }
 
 form.addEventListener("submit", searchAddressHandler);
