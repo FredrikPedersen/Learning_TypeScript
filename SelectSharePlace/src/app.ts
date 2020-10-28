6490ada301e6a0fce03602ef0ece2ab981223dbc
FredrikPedersen <fredrikhp@gmail.com>
2020-10-28 18:15:29 +0100

    Completed Section 13: Part 169: Getting User Input

--- SelectSharePlace/src/app.ts
+++ SelectSharePlace/src/app.ts
@@ -1,1 +1,9 @@
-console.log("Hello There");
+const form = document.querySelector("form")! as HTMLFormElement;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault();
+    const enteredAdress = addressInput.value;
+}
+
+form.addEventListener("submit", searchAddressHandler);
