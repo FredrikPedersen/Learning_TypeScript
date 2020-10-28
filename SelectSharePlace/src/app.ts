f6c2d13f2490e43134c01544393dd80bb7a7d075
FredrikPedersen <fredrikhp@gmail.com>
2020-10-28 18:44:51 +0100

    Completed Section 13: Part 170: Setting Up a Google API Key

--- SelectSharePlace/src/app.ts
+++ SelectSharePlace/src/app.ts
@@ -1,5 +1,4 @@
 const API_KEY = process.env.API_KEY;
-
 const form = document.querySelector("form")! as HTMLFormElement;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
